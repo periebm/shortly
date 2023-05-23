b1aaf146f943778c80078c61363c49ace9c9b976
Pericles <periclesebmendes@gmail.com>
2023-05-22 21:01:42 -0300

    feat: ranking

--- src/controllers/ranking.controllers.js
+++ src/controllers/ranking.controllers.js
@@ -4,9 +4,15 @@ import { db } from "../database/database.config.js";
 export async function getRanking(req, res){
 
     try {
-
-        res.status(200).send({
-        })
+        const rank = await db.query(`
+        SELECT COUNT(*) AS "linksCount", urls."userId", users.name AS name, SUM(urls.views) AS "visitCount"
+        FROM urls 
+        LEFT JOIN users ON users.id = urls."userId"
+        GROUP BY urls."userId", users.name
+        ORDER BY "visitCount" DESC
+        LIMIT 10;
+`)
+        res.status(200).send(rank.rows)
     } catch (err) {
         res.status(500).send(err.message);
     }
